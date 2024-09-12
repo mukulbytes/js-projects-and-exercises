8e4b15917a9d535b64c49dbd2f8cfc67f693e045
Mukul Dogra <mukulwritescode@gmail.com>
2024-09-13 03:08:37 +0530

    Set autoplay frequncy to 1s and formatted code in the Rock Paper Scissors Project

--- Projects/02-Rock-Paper-Scissors-Project/src/script.js
+++ Projects/02-Rock-Paper-Scissors-Project/src/script.js
@@ -112,10 +112,8 @@ function autoPlay() {
         isAutoPlaying = true;
         intervalIdAutoPlay = setInterval(() => {
             calculateComputerMove();
-
-            console.log(compMove);
             calculateOutcome(compMove);
-        }, 2000);
+        }, 1000);
     }
     else {
         clearInterval(intervalIdAutoPlay);
